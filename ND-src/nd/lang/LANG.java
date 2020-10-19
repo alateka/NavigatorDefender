a93a5e0ce572a2f20c5c942f1bd420f39b1ae94c
ALBERTO PÉREZ FRUCTUOSO <alateka@vivaldi.net>
2020-10-19 19:00:13 +0200

    Removed some void lines

--- ND-src/nd/lang/LANG.java
+++ ND-src/nd/lang/LANG.java
@@ -1,14 +1,12 @@
 package nd.lang;
 
 public class LANG {
-
 	
 	private String propertiesFileGenerated;
 	private String NavigatorDefenderBackground;
 	private String yourDownloadedFile;
 	private String itHasNotViruses;
 	private String detectedVirusesOn;
-	
 	
 	/**
 	 * Constructor
@@ -27,51 +25,41 @@ public class LANG {
 		this.detectedVirusesOn = detectedVirusesOn;
 	}
 
-
 	public String getPropertiesFileGenerated() {
 		return propertiesFileGenerated;
 	}
-
 
 	public void setPropertiesFileGenerated(String propertiesFileGenerated) {
 		this.propertiesFileGenerated = propertiesFileGenerated;
 	}
 
-
 	public String getNavigatorDefenderBackground() {
 		return NavigatorDefenderBackground;
 	}
-
 
 	public void setNavigatorDefenderBackground(String NavigatorDefenderBackground) {
 		this.NavigatorDefenderBackground = NavigatorDefenderBackground;
 	}
 
-
 	public String getYourDownloadedFile() {
 		return yourDownloadedFile;
 	}
-
 
 	public void setYourDownloadedFile(String yourDownloadedFile) {
 		this.yourDownloadedFile = yourDownloadedFile;
 	}
 
-
 	public String getItHasNotViruses() {
 		return itHasNotViruses;
 	}
-
 
 	public void setItHasNotViruses(String itHasNotViruses) {
 		this.itHasNotViruses = itHasNotViruses;
 	}
 
-
 	public String getDetectedVirusesOn() {
 		return detectedVirusesOn;
 	}
-
 
 	public void setDetectedVirusesOn(String detectedVirusesOn) {
 		this.detectedVirusesOn = detectedVirusesOn;
